119884db2e260de0d5b0c28c34f4f1a72c54f9dc
aambrose-unh <aambrose.unh@gmail.com>
2023-09-30 19:23:18 -0400

    playing with logger config

--- production/getInfo.py
+++ production/getInfo.py
@@ -11,9 +11,18 @@ import datetime
 
 import logging
 
-logging.basicConfig()
-
+# Logging config
 logger = logging.Logger("get_info")
+# Create handlers
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.INFO)
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+# Add handlers to the logger
+logger.addHandler(c_handler)
+
+logger.info("Test logger")
 
 databaseUsername = os.getenv("DB_USER")
 databasePassword = os.getenv("DB_PASSWD")
